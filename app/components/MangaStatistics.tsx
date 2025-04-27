6a9a4a51f287c0a520063369430035e7464050bc
chiron <chiron12@bu.edu>
2025-04-27 18:27:32 -0400

    adding favorite button in the page.tsx inside manga[id}

--- app/components/MangaStatistics.tsx
+++ app/components/MangaStatistics.tsx
@@ -7,16 +7,16 @@ interface Rating {
 
 export default function MangaStatistics({ rating, follows }: { rating: Rating; follows: number }) {
   return (
-    <div className="bg-gray-100 p-4 rounded shadow-md mt-4">
-      <h2 className="text-xl font-semibold mb-2">Statistics</h2>
-      <ul>
-        <li>
+    <div className="bg-gray-200 dark:bg-gray-800 p-4 rounded-lg shadow-md mt-6">
+      <h2 className="text-2xl font-semibold mb-4 text-center">Statistics</h2>
+      <ul className="space-y-2">
+        <li className="text-center">
           <span className="font-bold">Mean Rating:</span> {rating.average}
         </li>
-        <li>
+        <li className="text-center">
           <span className="font-bold">Bayesian Rating:</span> {rating.bayesian}
         </li>
-        <li>
+        <li className="text-center">
           <span className="font-bold">Follows:</span> {follows}
         </li>
       </ul>
