1f1cb79627c41da279df27cf0c65f5e0446be6a1
chiron <chiron12@bu.edu>
2025-04-28 23:33:37 -0400

    ew

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,17 @@
-import SearchForm from '../components/SearchForm';
+// app/search/page.tsx
+// Author: [Chiron]
+// Purpose: Renders the Search page where users can search for manga titles using the SearchForm component.
 
+import SearchForm from '../components/SearchForm'; // Import the manga search form component
+
+// Main page component for "/search" route
 export default function SearchPage() {
   return (
     <main className="p-8">
+      {/* Page Title */}
       <h1 className="text-2xl mb-4">Manga Search</h1>
+
+      {/* Render the search form */}
       <SearchForm />
     </main>
   );
